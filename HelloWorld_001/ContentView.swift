fb867b73c65ba21b7179f903ea6f2e56ec87d136
Tommy Yeargin <yeargin2021@gmail.com>
2021-08-26 22:14:19 -0400

    My first macOS program (?)!

---
+++ HelloWorld_001/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HelloWorld_001
+//
+//  Created by TOMMY YEARGIN on 8/26/21.
+//  ha
+//  byw
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld_001/HelloWorld_001App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld_001App.swift
+//  HelloWorld_001
+//
+//  Created by TOMMY YEARGIN on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
